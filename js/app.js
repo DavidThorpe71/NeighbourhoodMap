1f8d1cc76283c34688f2447235a8ccb5f29699d5
David Thorpe <davidthorpe878@gmail.com>
2017-06-19 21:11:54 +0100

    implemented sidenav

--- js/app.js
+++ js/app.js
@@ -1,3 +1,16 @@
+// side menu bar
+/* Set the width of the side navigation to 250px and the left margin of the page content to 250px */
+function openNav() {
+    document.getElementById("sidenav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+}
+
+/* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+}
+
 // twitter api
 window.twttr = (function(d, s, id) {
 	var js, fjs = d.getElementsByTagName(s)[0],
